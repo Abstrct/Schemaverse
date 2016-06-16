a69883b98fe7d07888865048064a40e08fecad34
Christopher Browne <cbbrowne@ca.afilias.info>
2016-06-16 16:59:34 -0400

    Indentify the ticker

--- tic.pl
+++ tic.pl
@@ -12,95 +12,95 @@ use DBI;
 #My quick off switch
 while (1){ 
 
-# Config Variables
-my $db_name 	= "schemaverse";
-my $db_username = "schemaverse";
+	# Config Variables
+	my $db_name 	= "schemaverse";
+	my $db_username = "schemaverse";
 
-# Make the master database connection
-my $master_connection = DBI->connect("dbi:Pg:dbname=${db_name};host=localhost", $db_username);
+	# Make the master database connection
+	my $master_connection = DBI->connect("dbi:Pg:dbname=${db_name};host=localhost", $db_username);
 
-$master_connection->do('SELECT ROUND_CONTROL();');
+	$master_connection->do('SELECT ROUND_CONTROL();');
 
-# Move the rest of the ships in whatever direction they have specified
-my $sql = <<SQLSTATEMENT;
-BEGIN WORK;
-LOCK TABLE ship, ship_control IN EXCLUSIVE MODE;
- SELECT MOVE_SHIPS();
-COMMIT WORK;
-SQLSTATEMENT
+	# Move the rest of the ships in whatever direction they have specified
+	my $sql = <<SQLSTATEMENT;
+	BEGIN WORK;
+	LOCK TABLE ship, ship_control IN EXCLUSIVE MODE;
+	SELECT MOVE_SHIPS();
+	COMMIT WORK;
+	SQLSTATEMENT
 
-$master_connection->do($sql); 
+		$master_connection->do($sql); 
 
-#my $sql = <<SQLSTATEMENT;
-#SELECT update_ships_near_ships();
-#SELECT update_ships_near_planets();
-#SQLSTATEMENT
-#$master_connection->do($sql); 
+	#my $sql = <<SQLSTATEMENT;
+	#SELECT update_ships_near_ships();
+	#SELECT update_ships_near_planets();
+	#SQLSTATEMENT
+	#$master_connection->do($sql); 
 
 
-# Retreive Fleet Scripts and run them as the user they belong to
-my $sql = <<SQLSTATEMENT;
-SELECT 
-	player.id as player_id,
-	player.username as username,
-	fleet.id as fleet_id,
-	player.error_channel as error_channel
-FROM 
-	fleet, player
-WHERE
-	fleet.player_id=player.id
-	AND
-	fleet.enabled='t'
-	AND 
-	fleet.runtime > '0 minutes'::interval
-ORDER BY 
-	player.username;
-SQLSTATEMENT
+	# Retreive Fleet Scripts and run them as the user they belong to
+	my $sql = <<SQLSTATEMENT;
+	SELECT 
+		player.id as player_id,
+		player.username as username,
+		fleet.id as fleet_id,
+		player.error_channel as error_channel
+		FROM 
+		fleet, player
+		WHERE
+		fleet.player_id=player.id
+		AND
+		fleet.enabled='t'
+		AND 
+		fleet.runtime > '0 minutes'::interval
+		ORDER BY 
+		player.username;
+	SQLSTATEMENT
 
-my $fleet_fail_event = $master_connection->prepare("INSERT INTO event(tic,action,player_id_1,referencing_id,descriptor_string) VALUES ((SELECT last_value FROM tic_seq),'FLEET_FAIL',?,?,?)");
-my $rs = $master_connection->prepare($sql); 
-$rs->execute();
-$temp_user = '';
-while (($player_id, $player_username, $fleet_id, $error_channel) = $rs->fetchrow()) {
+		my $fleet_fail_event = $master_connection->prepare("INSERT INTO event(tic,action,player_id_1,referencing_id,descriptor_string) VALUES ((SELECT last_value FROM tic_seq),'FLEET_FAIL',?,?,?)");
+	my $rs = $master_connection->prepare($sql); 
+	$rs->execute();
+	$temp_user = '';
+	while (($player_id, $player_username, $fleet_id, $error_channel) = $rs->fetchrow()) {
 
-	if ($temp_user ne $player_username)
-	{
-		if ($temp_user ne '')
+		if ($temp_user ne $player_username)
 		{
-			$temp_connection->disconnect();
-			
-		}
-		$temp_user = $player_username;
-		$temp_connection = DBI->connect("dbi:Pg:dbname=$db_name;host=localhost", $player_username);
-		$temp_connection->{PrintError} = 0;
-		$temp_connection->{RaiseError} = 1;
-	}
-	#$temp_connection->{application_name} = $fleet_id;
-	$temp_connection->do("SET application_name TO ${fleet_id}");
-	eval { $temp_connection->do("SELECT RUN_FLEET_SCRIPT(${fleet_id})"); };
-  	if( $@ ) {
-		$temp_connection->do("NOTIFY ${error_channel}, 'Fleet script ${fleet_id} has failed to fully execute during the tic'; ");
-#		$fleet_fail_event->execute($player_id,$fleet_id,$@);
-	}
-}
-if ($temp_user ne '') {
-	$temp_connection->disconnect();
-}
-$rs->finish;
+			if ($temp_user ne '')
+			{
+				$temp_connection->disconnect();
 
-my $sql = <<SQLSTATEMENT;
-BEGIN WORK;
-LOCK TABLE ship, ship_control IN EXCLUSIVE MODE;
-SELECT
+			}
+			$temp_user = $player_username;
+			$temp_connection = DBI->connect("dbi:Pg:dbname=$db_name;host=localhost", $player_username);
+			$temp_connection->{PrintError} = 0;
+			$temp_connection->{RaiseError} = 1;
+		}
+		#$temp_connection->{application_name} = $fleet_id;
+		$temp_connection->do("SET application_name TO ${fleet_id}");
+		eval { $temp_connection->do("SELECT RUN_FLEET_SCRIPT(${fleet_id})"); };
+		if( $@ ) {
+			$temp_connection->do("NOTIFY ${error_channel}, 'Fleet script ${fleet_id} has failed to fully execute during the tic'; ");
+			#		$fleet_fail_event->execute($player_id,$fleet_id,$@);
+		}
+	}
+	if ($temp_user ne '') {
+		$temp_connection->disconnect();
+	}
+	$rs->finish;
+
+	my $sql = <<SQLSTATEMENT;
+	BEGIN WORK;
+	LOCK TABLE ship, ship_control IN EXCLUSIVE MODE;
+	SELECT
         CASE
-                WHEN ship_control.action = 'ATTACK' THEN ATTACK(ship.id, ship_control.action_target_id)::integer
-                WHEN ship_control.action = 'REPAIR' THEN REPAIR(ship.id, ship_control.action_target_id)::integer
-                WHEN ship_control.action = 'MINE' THEN MINE(ship.id, ship_control.action_target_id)::integer
-                ELSE NULL END
-FROM
+		WHEN ship_control.action = 'ATTACK' THEN ATTACK(ship.id, ship_control.action_target_id)::integer
+		WHEN ship_control.action = 'REPAIR' THEN REPAIR(ship.id, ship_control.action_target_id)::integer
+		WHEN ship_control.action = 'MINE' THEN MINE(ship.id, ship_control.action_target_id)::integer
+		ELSE NULL END
+		FROM
         ship, ship_control
-WHERE
-         ship.id = ship_control.ship_id
+		WHERE
+		ship.id = ship_control.ship_id
         AND
         ship_control.action IS NOT NULL
         AND
@@ -109,21 +109,21 @@ WHERE
         ship.destroyed='f'
         AND 
         ship.last_action_tic != (SELECT last_value FROM tic_seq);
-COMMIT WORK;
-SQLSTATEMENT
-$master_connection->do($sql);
+	COMMIT WORK;
+	SQLSTATEMENT
+		$master_connection->do($sql);
 
 
 
 
-#planets are mined
-$master_connection->do("lock table planet_miners;SELECT perform_mining();");
+	#planets are mined
+	$master_connection->do("lock table planet_miners;SELECT perform_mining();");
 
-#dirty planet renewal hack
-$master_connection->do("UPDATE planet SET fuel=fuel+1000000 WHERE id in (select id from planet where fuel < 10000000 order by RANDOM() LIMIT 5000);");
-	
-#future_health is dealt with
-$master_connection->do("BEGIN WORK; 
+	#dirty planet renewal hack
+	$master_connection->do("UPDATE planet SET fuel=fuel+1000000 WHERE id in (select id from planet where fuel < 10000000 order by RANDOM() LIMIT 5000);");
+
+	#future_health is dealt with
+	$master_connection->do("BEGIN WORK; 
 LOCK TABLE ship, ship_control IN EXCLUSIVE MODE; 
 create temp table t_ship_updates (id integer, current_health integer, future_health integer, max_health integer, last_living_tic integer, destroyed boolean, player_id integer);
 insert into t_ship_updates (id, current_health, future_health, max_health, last_living_tic, destroyed, player_id)
@@ -143,19 +143,19 @@ cluster ship_pkey on ship;
 COMMIT WORK;");
 
 
-$master_connection->do("vacuum ship;");
+	$master_connection->do("vacuum ship;");
 
 
-#Update some stats now and then
-#$master_connection->do("insert into stat_log  select * from current_stats WHERE mod(current_tic,5)=0;");
+	#Update some stats now and then
+	#$master_connection->do("insert into stat_log  select * from current_stats WHERE mod(current_tic,5)=0;");
 
-$master_connection->do("INSERT INTO event(player_id_1, action, tic, public) VALUES(0,'TIC',(SELECT last_value FROM tic_seq),'t')");
+	$master_connection->do("INSERT INTO event(player_id_1, action, tic, public) VALUES(0,'TIC',(SELECT last_value FROM tic_seq),'t')");
 
-#Tic is increased to NEXTVAL
-$master_connection->do("SELECT nextval('tic_seq')");	
+	#Tic is increased to NEXTVAL
+	$master_connection->do("SELECT nextval('tic_seq')");	
 
 
-$master_connection->disconnect();
-sleep(60);
+	$master_connection->disconnect();
+	sleep(60);
 
 }
